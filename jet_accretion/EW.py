bc3500434c01d72cb4135d203ec33aa37bbe1445
dylanb <dylanbollendb@gmail.com>
2019-10-04 14:40:51 +1000

    changed from using median values to using max values

--- jet_accretion/EW.py
+++ jet_accretion/EW.py
@@ -4,39 +4,60 @@ import sys
 import os
 
 
-def equivalent_width(wav, fl, cut=False, wave_min=0, wave_max=0):
+def equivalent_width(wav, fl, cut=False, wave_min=0, wave_max=0,use_median=False):
     """
     Determine the equivalent width of a line
     """
     wave = np.array(wav)
     flux = np.array(fl)
-    if cut==False:
+    if use_median==True:
+        if cut==False:
 
-        # Use the whole array to compute the equivalent width
-        # The median value is the continuum level
-        median_low  = np.median(flux[0:50])
-        median_high = np.median(flux[-50:-1])
-        median      = ( median_low + median_high ) / 2
+            # Use the whole array to compute the equivalent width
+            # The median value is the continuum level
+            median_low  = np.median(flux[0:50])
+            median_high = np.median(flux[-50:-1])
+            median      = ( median_low + median_high ) / 2
 
-        delta_wave  = (wave[2:] - wave[:-2]) / 2.
-        integration = (1 - flux[1:-1]/median) * delta_wave
-        EW          = np.sum(integration)
+            delta_wave  = (wave[2:] - wave[:-2]) / 2.
+            integration = (1 - flux[1:-1]/median) * delta_wave
+            EW          = np.sum(integration)
+
+        else:
+            # Cut the wave and flux arrays to the region for which you want to
+            # calculate the equivalent width
+
+            indices     = np.where((wave_min < wave) & (wave < wave_max))
+            wave_cut    = wave[indices]
+            flux_cut    = flux[indices]
+
+            median_low  = np.median(flux_cut[0:50])
+            median_high = np.median(flux_cut[-50:-1])
+            median      = ( median_low + median_high ) / 2
+
+            delta_wave  = (wave_cut[2:] - wave_cut[:-2]) / 2.
+            integration = (1 - flux_cut[1:-1]/median) * delta_wave
+            EW          = np.sum(integration)
 
     else:
-        # Cut the wave and flux arrays to the region for which you want to
-        # calculate the equivalent width
+        if cut==False:
 
-        indices     = np.where((wave_min < wave) & (wave < wave_max))
-        wave_cut    = wave[indices]
-        flux_cut    = flux[indices]
+            # Use the whole array to compute the equivalent width
+            delta_wave  = (wave[2:] - wave[:-2]) / 2.
+            integration = (1 - flux[1:-1]) * delta_wave
+            EW          = np.sum(integration)
 
-        median_low  = np.median(flux_cut[0:50])
-        median_high = np.median(flux_cut[-50:-1])
-        median      = ( median_low + median_high ) / 2
+        else:
+            # Cut the wave and flux arrays to the region for which you want to
+            # calculate the equivalent width
 
-        delta_wave  = (wave_cut[2:] - wave_cut[:-2]) / 2.
-        integration = (1 - flux_cut[1:-1]/median) * delta_wave
-        EW          = np.sum(integration)
+            indices     = np.where((wave_min < wave) & (wave < wave_max))
+            wave_cut    = wave[indices]
+            flux_cut    = flux[indices]
+
+            delta_wave  = (wave_cut[2:] - wave_cut[:-2]) / 2.
+            integration = (1 - flux_cut[1:-1]) * delta_wave
+            EW          = np.sum(integration)
 
 
     return EW
--- jet_accretion/scale_intensity.py
+++ jet_accretion/scale_intensity.py
@@ -3,7 +3,7 @@ A function to rescale the flux/intensity level of the spectra by fitting
 a straight line to the template.
 '''
 import numpy as np
-
+import matplotlib.pylab as plt
 
 def scale_intensity(wave_0,
                     wave_template,
@@ -22,10 +22,30 @@ def scale_intensity(wave_0,
         wave_min_b     = wave_0 - (wave_0_d)
         wave_max_a     = wave_0 + (wave_0_d)
         wave_max_b     = wave_0 + (wave_0_d + wave_region)
-        median_I_low   = np.median(intensity_template[np.where((wave_template > wave_min_a) & (wave_template < wave_min_b))])
-        median_I_high  = np.median(intensity_template[np.where((wave_template > wave_max_a) & (wave_template < wave_max_b))])
-        gradient       = (median_I_high - median_I_low) / ( 2 * wave_0_d + wave_region)
-        intercept      = median_I_high - gradient *  (wave_0 + wave_0_d + 0.5 * wave_region)
+        index_low  = np.argmax(intensity_template[np.where((wave_template > wave_min_a) & (wave_template < wave_min_b))])
+        index_high = np.argmax(intensity_template[np.where((wave_template > wave_max_a) & (wave_template < wave_max_b))])
+        I_low      = (intensity_template[np.where((wave_template > wave_min_a) & (wave_template < wave_min_b))])[index_low]
+        I_high     = (intensity_template[np.where((wave_template > wave_max_a) & (wave_template < wave_max_b))])[index_high]
+        wave_low   = (wave_template[np.where((wave_template > wave_min_a) & (wave_template < wave_min_b))])[index_low]
+        wave_high  = (wave_template[np.where((wave_template > wave_max_a) & (wave_template < wave_max_b))])[index_high]
+        gradient       = (I_high - I_low) / ( wave_high - wave_low)
+        intercept      = I_high - gradient * wave_high
         scaling_interp = np.poly1d(np.array([gradient, intercept]))
         intensity_spec = intensity_spec * scaling_interp(wave_spec)
-    return intensity_spec
+
+    # if norm==True:
+    #     wave_min_a     = wave_0 - (wave_0_d + wave_region)
+    #     wave_min_b     = wave_0 - (wave_0_d)
+    #     wave_max_a     = wave_0 + (wave_0_d)
+    #     wave_max_b     = wave_0 + (wave_0_d + wave_region)
+    #     median_I_low   = np.median(intensity_template[np.where((wave_template > wave_min_a) & (wave_template < wave_min_b))])
+    #     median_I_high  = np.median(intensity_template[np.where((wave_template > wave_max_a) & (wave_template < wave_max_b))])
+    #     gradient       = (median_I_high - median_I_low) / ( 2 * wave_0_d + wave_region)
+    #     intercept      = median_I_high - gradient *  (wave_0 + wave_0_d + 0.5 * wave_region)
+    #     scaling_interp = np.poly1d(np.array([gradient, intercept]))
+    #     intensity_spec = intensity_spec * scaling_interp(wave_spec)
+    #     print(gradient, intercept)
+    #     plt.plot(wave_spec, intensity_spec)
+    #     plt.plot(wave_template, intensity_template)
+    #     plt.show()
+    return intensity_spec, scaling_interp
